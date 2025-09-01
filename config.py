9b90a75eb6a44a04bcbaabee09ab826d4dcce3ee
Abineet <95118605+ab1neet@users.noreply.github.com>
2025-09-01 13:45:21 +0530

    .

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ CONFIG = {
     'sc_data_path': 'SC.csv',
     'sc2_data_path': 'SC2.csv',
     'hospital_address': "Medical College, Trivandrum, Kerala, India",
-    'openweathermap_api_key': "9d93cff80115b3152f8732f2679b738c",
-    'tomtom_api_key': "4fkroF84IrwfpSZOmpatPDHUbr1GNih8"
+    'openweathermap_api_key': "open_weather_api",
+    'tomtom_api_key': "tomtom_api"
+
 }
